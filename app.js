9c10dde58dc39ccf23de40350af2e36da867124f
James Kao <jmk17@cornell.edu>
2020-05-23 13:39:32 -0700

    Added final global catches to prepare for production

--- app.js
+++ app.js
@@ -16,15 +16,25 @@ const logging = new Logging({projectId});
 const log = logging.log(nconf.get('gcp_log_name'));
 async function notice(msg) {
   log.notice(logging.entry(null, msg));
-  console.log(msg);
+  if (msg.message) {
+    console.log(msg.message);
+  } else {
+    console.log(msg);
+  }
 }
 async function info(msg) {
   log.info(logging.entry(null, msg));
+}
+async function warn(msg) {
+  log.warn(logging.entry(null, msg));
+  console.log(msg);
 }
 async function error(msg) {
   log.error(logging.entry(null, msg));
   console.log(msg);
 }
+
+notice({service: 'INIT', method: 'logging', message: 'DeepL Translator Bot Launched'});
 
 const prefix = nconf.get('prefix');
 const deepl_auth_key = nconf.get('deepl_auth_key');
@@ -35,13 +45,15 @@ var cross_channels = {};
 
 const client = new Discord.Client();
 var cldIdentifier;
+
 cld3.loadModule().then(factory => {
   cldIdentifier = factory.create(0,700);
+  notice({service: 'INIT', method: 'cld3-asm', message: 'loadModule() factory created'});
   client.login(nconf.get('discord_bot_token'));
 });
 
 client.once('ready', () => {
-	notice('DeepL Translator Bot Ready');
+	notice({service: 'INIT', method: 'discord', message: 'client connection ready'});
  
   client.user.setActivity(nconf.get('auto_trans_channel'));
 
@@ -53,44 +65,69 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.author.bot) {
-    return;
-  }
+  handleMessage(message).catch(err => {
+    error({service: 'ONMESSAGE', method: 'on', msgId: message.id, message: err.message, details: err});
+  });
+});
 
-  let cmd;
-  let text;
-  let replyChannel;
-
-  if (message.content.startsWith(prefix)) {
-    const cmdEndIndex = message.content.indexOf(' ');
-
-    cmd = message.content.substring(1, cmdEndIndex).toLowerCase();
-    text = message.content.substring(cmdEndIndex + 1);
-    replyChannel = message.channel;
-  } else if (cross_channels[message.channel.id] != null) {
-    replyChannel = cross_channels[message.channel.id];
-    text = message.content;
-    if (cldIdentifier.findLanguage(text).language === 'ja') {
-      cmd = 'je'
-    } else {
-      cmd = 'ej'
+async function handleMessage(message) {
+  try {
+    if (message.author.bot) {
+      return;
     }
+  
+    let cmd;
+    let text;
+    let replyChannel;
+  
+    if (message.content.startsWith(prefix)) {
+      const cmdEndIndex = message.content.indexOf(' ');
+  
+      cmd = message.content.substring(1, cmdEndIndex).toLowerCase();
+      text = message.content.substring(cmdEndIndex + 1);
+      replyChannel = message.channel;
+  
+      info({service: 'ONMESSAGE', method: 'command', msgId: message.id, message: cmd});
+    } else if (cross_channels[message.channel.id] != null) {
+      replyChannel = cross_channels[message.channel.id];
+      text = message.content;
+  
+      let src_ident = cldIdentifier.findLanguage(text);
+      let src_lang = src_ident.language;
+      if (src_lang === 'ja' || src_lang === 'zh') {
+        cmd = 'je'
+      } else {
+        cmd = 'ej'
+      }
+      info({service: 'ONMESSAGE', method: 'cld3-asm-detect', msgId: message.id, from_lang: src_lang, details: src_ident});
+    } else {
+      return;
+    }
+  
+    info({service: 'ONMESSAGE', method: 'discord-message', msgId: message.id, author: message.author.username, channel: `${replyChannel.name}`, message: message.content, details: message.toJSON()});
+   
+    if (cmd === 'ej') {
+      handleTranslationReply(text, 'JA', message, replyChannel);
+    } else if (cmd === 'je') {
+      handleTranslationReply(text, 'EN', message, replyChannel);
+    }  
+  } catch {
+    warn({service: 'ONMESSAGE', method: 'handleMessage', msgId: message.id, message: err.message, details: err});
   }
- 
-  if (cmd === 'ej') {
-    info(message.toJSON());
-    translate(text, 'JA', message.author).then( result => {
-      replyChannel.send(result);
-    });
-  } else if (cmd === 'je') {
-    info(message.toJSON());
-    translate(text, 'EN', message.author).then( result => {
-      replyChannel.send(result);
-    });
-  }
-})
+}
 
-async function translate(text, lang, author) {
+async function handleTranslationReply(text, target_lang, message, replyChannel) {
+  try {
+    replyChannel.startTyping();
+    const result = await translate(text, target_lang, message)
+    replyChannel.send(result);
+    replyChannel.stopTyping();
+  } catch (err) {
+    warn({service: 'TRANSLATE', method: 'handleTranslationReply', msgId: message.id, message: err.message, details: err});
+  }
+}
+
+async function translate(text, lang, message) {
   let matches = text.matchAll(/(<[^\s]+>)/g);
   let escapes = [];
   for (const match of matches) {
@@ -98,35 +135,41 @@ async function translate(text, lang, author) {
     text = text.replace(match[0], esc+escapes.length);
   }
 
-  let result = await deeplTranslate(text, lang);
+  let result = await deeplTranslate(text, lang, message);
 
   if (result == null || result.length == 0) {
-    return result;
+    result = "<Empty translation returned from service>";
+  } else if (result.length > 2000) {
+    result = "<Translation too large for Discord message>";
   }
 
   for (let i=1;i<=escapes.length;i++) {
     result = result.replace(esc+i, escapes[i-1]);
   }
 
-  return `**@${author.username}** ${result}`;
+  return `\`\`\`${message.author.username}: ${result}\`\`\``;
 }
 
-async function deeplTranslate(text, targetLang) {
+async function deeplTranslate(text, targetLang, message) {
   try {
     const request = {
       auth_key: deepl_auth_key,
       target_lang: targetLang,
       text: text,
     };
-    info(request);
+    info({service: 'TRANSLATE', method: 'deepl-request', msgId: message.id, to_lang: targetLang, message: text, length: text.length, details: request});
     const response = await axios.post(deepl_url, qs.stringify(request))
-    //console.log(response);
     client.user.setActivity("DeepL");
   
-    info(response.data.translations[0]);
+    info({service: 'TRANSLATE',
+      method: 'deepl-response',
+      msgId: message.id,
+      from_lang: response.data.translations[0].detected_source_language,
+      message: response.data.translations[0].text,
+    });
     return response.data.translations[0].text;  
   } catch (err) {
-    error(err.toJSON());
-    return "Error...";
+    warn({service: 'TRANSLATE', method: 'deepl-response', msgId: message.id, message: err.message, details: err});
+    return "<Error returned from deepl>";
   }
 }
